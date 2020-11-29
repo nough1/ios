b9d0d8074d215beeb36900c00f4a81fea3b10102
guoan <1naouguh@gmail.com>
2020-11-29 16:23:22 +0800

    fix

---
+++ 20201129/HelloXCode/HelloXCode/HelloXCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloXCodeApp.swift
+//  HelloXCode
+//
+//  Created by guoan hu on 2020/11/29.
+//
+
+import SwiftUI
+
+@main
+struct HelloXCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
